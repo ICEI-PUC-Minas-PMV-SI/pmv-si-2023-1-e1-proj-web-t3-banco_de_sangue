835048e507ce1dc6ed5cbd4b42cb0b06d89e7989
Bruno Tetzner <brunoltetzner@gmail.com>
2023-06-03 17:17:36 -0300

    feat: responsive donor screen

---
+++ src/pages/donor-screen/script-checkbox.js
@@ -0,0 +1,10 @@
+function uncheckOther(checkbox) {
+  if (checkbox.checked) {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(function(el) {
+      if (el !== checkbox) {
+        el.checked = false;
+      }
+    });
+  }
+}
