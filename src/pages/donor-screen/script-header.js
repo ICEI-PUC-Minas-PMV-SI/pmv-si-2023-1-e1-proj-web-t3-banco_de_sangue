de4add4459221185f5debf7b213dde6321d70699
Bruno Tetzner <brunoltetzner@gmail.com>
2023-06-04 11:19:57 -0300

    fix: modals

---
+++ src/pages/donor-screen/script-header.js
@@ -0,0 +1,2 @@
+const getText = document.getElementsByClassName('main-header__icon__text')[0];
+getText.innerHTML = 'Maria'
--- src/pages/donor-screen/script-modal.js
+++ src/pages/donor-screen/script-modal.js
@@ -1,32 +1,62 @@
+//MODAL DE EDITAR CONTA
+
 var openModalButton = document.getElementById("open-modal");
-var modal = document.getElementById("modal");
+var modal = document.getElementById("modal-edit-account");
 
 openModalButton.addEventListener("click", function() {
   modal.style.display = "block";
 });
-
-
-
 window.addEventListener("click", function(event) {
   if (event.target === modal) {
     modal.style.display = "none";
   }
 });
 
+//MODAL DE ENCERRAR CONTA
+
 var openModalButtonDelete = document.getElementById("open-modal-delete-account");
 var modalDelete = document.getElementById("modal-delete-account");
-var closeButtonDelete = document.getElementsByClassName("close")[0];
+
+var closeButtonDelete = document.getElementById("close-delete-modal-cancel");
+var closeButtonDeleteAndConfirm = document.getElementById("close-delete-modal-confirm");
+
 
 openModalButtonDelete.addEventListener("click", function() {
   modalDelete.style.display = "block";
 });
 
+
 closeButtonDelete.addEventListener("click", function() {
+  modalDelete.style.display = "none";
+});
+closeButtonDeleteAndConfirm.addEventListener("click", function() {
   modalDelete.style.display = "none";
 });
 
 window.addEventListener("click", function(event) {
   if (event.target === modalDelete) {
     modalDelete.style.display = "none";
+  }
+});
+
+
+//MODAL ABRIR MENU DE NAVEGAÇÃO
+var openModalNavegate = document.getElementById("open-modal-navigate");
+var modalNavigate = document.getElementById("modal-navigate");
+
+openModalNavegate.addEventListener("click", function() {
+  modalNavigate.style.display = "block";
+});
+
+var closeNavigate = document.getElementsByClassName("lateral-menu-content__close")[0];
+console.log(closeNavigate,'closeNavigate')
+closeNavigate.addEventListener("click", function() {
+  modalNavigate.style.display = "none";
+});
+
+
+window.addEventListener("click", function(event) {
+  if (event.target === modalNavigate) {
+    modalNavigate.style.display = "none";
   }
 });
