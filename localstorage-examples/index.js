60468710ef3566175c884c90ab918a0d7c17f1ce
Gilberto-Modesto <128002028+Gilberto-Modesto@users.noreply.github.com>
2023-06-25 09:12:52 -0300

    nova atualização do projeto

--- localstorage-examples/index.js
+++ localstorage-examples/index.js
@@ -1,4 +1,4 @@
-// Função que gera um id unico para o usuário
+'// Função que gera um id unico para o usuário
 function generateUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0,
