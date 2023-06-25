03af6345300403e634ef658b24f0af50cf005bf4
Breno Xavier <brenoxavier8.bx@gmail.com>
2023-06-25 17:01:19 -0300

    feat: specify delete account modal styles

--- src/commons/interface/delete-acount-modal.js
+++ src/commons/interface/delete-acount-modal.js
@@ -5,11 +5,11 @@ const closeModal = () => {
 
 const _createDeleteAccountModal = (onConfirmClick) => {
   const modal = `
-  <div id="modal-delete-account" class="modal">
+  <div id="modal-delete-account" class="generic-modal">
       <div class="modal-delete-account__body">
         <div class="modal-delete-account__body__content">
-          <h1 class="modal-content__title">Apagar conta</h1>
-          <p class="modal-content__text">Tem certeza de que deseja apagar a conta?</p>    
+          <h1 class="generic-modal-content__title">Apagar conta</h1>
+          <p class="generic-modal-content__text">Tem certeza de que deseja apagar a conta?</p>    
           <div class="register-section__button-div">
             <button 
               id="close-delete-modal-cancel"
