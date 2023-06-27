0fc09af3fc7a6d3bc4b4ec4e8dc689e1f3fdce76
Bruno Tetzner <brunoltetzner@gmail.com>
2023-06-27 08:49:22 -0300

    fix"get correct logged  user at donor screen

--- src/pages/donor-screen/script-donor-info.js
+++ src/pages/donor-screen/script-donor-info.js
@@ -181,7 +181,9 @@ function deleteUser(userId) {
 
 // ============== RENDERIZAR INFORMAÇÕES DO DOADOR ===================
 
-const user  = getItemLocalStorage('base-blood-user-db')[0];
+const users  = getItemLocalStorage('base-blood-user-db');
+const userInfo  = getItemLocalStorage('base-blood-signin')
+const user = getUserById(userInfo.userId)
 const userFirstName = user.name.split(' ')[0]
 
 const mainMenuTitle = document.getElementsByClassName('main-menu__title')[0];
